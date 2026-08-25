67b0019a6ec5753d71570645929218c7f0bfd271
Keerthika-Venkatesh <keerthi2026jv@gmail.com>
2026-08-25 22:03:26 +0530

    Add files via upload

---
+++ jsvariable.js
@@ -0,0 +1,27 @@
+
+// 1. Favourite color
+const favouriteColor = "blue";
+
+// 2. BCA percentage
+const bcaPercentage = 78.5;
+
+// 3. var changed from 10 to 50
+var num = 10;
+num = 50;
+
+// 4. Two vars multiplied
+var num1 = 4;
+var num2 = 6;
+var product = num1 * num2;
+console.log(product); // 24
+
+// 5. price and quantity total
+const price = 19.99;
+const quantity = 3;
+const total = price * quantity;
+console.log(total); // 59.97
+
+// 6. var city changed and printed
+var city = "Mumbai";
+city = "Delhi";
+console.log(city); // Delhi
